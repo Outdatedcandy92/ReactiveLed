8c59aa4682384a9a2d73c5f804a2eae4cb2f7487
Outdatedcandy92 <138517406+Outdatedcandy92@users.noreply.github.com>
2024-08-24 00:10:52 -0400

    fix

--- music.ino
+++ music.ino
@@ -1,83 +1,52 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-#include <FS.h>
 
-const char* ssid = "";
-const char* password = "";
+const char *ssid = "";
+const char *password = "";
+
+IPAddress local_IP(192, 168, 2, 90);
+IPAddress gateway(192, 168, 2, 1);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress dns(8, 8, 8, 8);
 
 ESP8266WebServer server(80);
-File fsUploadFile;
-
-const int ledPin = D1;
 
 void setup() {
-    Serial.begin(115200);
-    pinMode(ledPin, OUTPUT);
-    digitalWrite(ledPin, LOW);
+    Serial.begin(9600);
+    Serial.println();
+    Serial.println("Starting WiFi connection...");
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    if (!WiFi.config(local_IP, gateway, subnet, dns)) {
+        Serial.println("STA Failed to configure");
+    }
 
     WiFi.begin(ssid, password);
+
+    int attempts = 0;
     while (WiFi.status() != WL_CONNECTED) {
         delay(1000);
-        Serial.println("Connecting to WiFi...");
+        Serial.print(".");
+        attempts++;
+        if (attempts > 30) {
+            Serial.println("Failed to connect to WiFi");
+            return;
+        }
     }
+    Serial.println();
     Serial.println("Connected to WiFi");
 
     Serial.print("IP Address: ");
     Serial.println(WiFi.localIP());
 
-    if (!SPIFFS.begin()) {
-        Serial.println("Failed to mount file system");
-        return;
-    }
-
-    server.on("/", HTTP_GET, handleRoot);
-    server.on("/upload", HTTP_POST, []() {
-        server.send(200);
-    }, handleFileUpload);
-
+    server.on("/", HTTP_GET, []() {
+        server.send(200, "text/html", "<!DOCTYPE html><html><body><h1>Hello, World!</h1></body></html>");
+    });
     server.begin();
     Serial.println("HTTP server started");
 }
 
 void loop() {
     server.handleClient();
-}
-
-void handleRoot() {
-    server.send(200, "text/html", "<form method='POST' action='/upload' enctype='multipart/form-data'><input type='file' name='file'><input type='submit' value='Upload'></form>");
-}
-
-void handleFileUpload() {
-    HTTPUpload& upload = server.upload();
-    static String filename;
-    if (upload.status == UPLOAD_FILE_START) {
-        filename = "/" + upload.filename;
-        if (!filename.startsWith("/")) filename = "/" + filename;
-        Serial.print("Upload File Name: "); Serial.println(filename);
-        fsUploadFile = SPIFFS.open(filename, "w");
-    } else if (upload.status == UPLOAD_FILE_WRITE) {
-        if (fsUploadFile) fsUploadFile.write(upload.buf, upload.currentSize);
-    } else if (upload.status == UPLOAD_FILE_END) {
-        if (fsUploadFile) fsUploadFile.close();
-        Serial.print("Upload Size: "); Serial.println(upload.totalSize);
-        server.send(200, "text/plain", "File Uploaded Successfully");
-        processAudioFile(filename.c_str());
-    }
-}
-
-void processAudioFile(const char* filename) {
-    File audioFile = SPIFFS.open(filename, "r");
-    if (!audioFile) {
-        Serial.println("Failed to open file for reading");
-        return;
-    }
-
-    while (audioFile.available()) {
-        int audioValue = audioFile.read();
-        int brightness = map(audioValue, 0, 255, 0, 1023);
-        analogWrite(ledPin, brightness);
-        delay(10);
-    }
-
-    audioFile.close();
 }
